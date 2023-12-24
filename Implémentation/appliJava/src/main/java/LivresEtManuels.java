a08212036564f5170cb8e2ee95fbf64ca8a04ccd
alessmcs <alessandramancas23@gmail.com>
2023-12-24 15:03:47 -0500

    update livresEtManuels

--- Implémentation/appliJava/src/main/java/LivresEtManuels.java
+++ Implémentation/appliJava/src/main/java/LivresEtManuels.java
@@ -72,7 +72,7 @@ public class LivresEtManuels extends Produit {
 						super.demanderAjoutPanier((Acheteur) util);
 						break;
 					case "5":
-						Catalogue.voirCatalogue(util);
+						Catalogue.voirCatalogue((Acheteur) util);
 					default:
 						System.out.println("Svp entrez une option valide!");
 				}
@@ -88,7 +88,7 @@ public class LivresEtManuels extends Produit {
 					String choix = s.nextLine();
 					switch(choix){
 						case "1" :
-							Catalogue.voirCatalogue(util);
+							Catalogue.voirCatalogue((Acheteur) util);
 							break;
 						case "0" :
 							Utilisateur.afficherMenu(util);
@@ -98,42 +98,5 @@ public class LivresEtManuels extends Produit {
 				}
 			} while (!validInput);
 		}
-
-//		System.out.print("Entrez votre choix: ");
-//
-//
-//		while(true){
-//			System.out.println("\nQue voulez-vous faire?");
-//			System.out.println("1. Voir les commentaires");
-//			System.out.println("2. Commenter et evaluer le produit");
-//			System.out.println("3. Liker le produit");
-//			System.out.println("4. Ajouter au panier le produit");
-//			System.out.println("5. Revenir au catalogue");
-//
-//			String choix = scanChoix.nextLine();
-//			switch(choix){
-//				case "1":
-//					if(enregistrerEvalComplete(ach) == null){
-//						System.out.println("Aucun commentaire");
-//					} else {
-//						super.voirEval(ach);
-//					}
-//					break;
-//				case "2":
-//					super.commenter(ach);
-//					super.evaluer();
-//					super.verifier(ach);
-//					break;
-//				case "3":
-//					super.liker();
-//					break;
-//				case "4":
-//					super.demanderAjoutPanier(ach);
-//					break;
-//				case "5":
-//					Catalogue.voirCatalogue(ach);
-//				default:
-//					System.out.println("Svp entrez une option valide!");
-//			}
 		}
 }
